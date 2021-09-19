56967ce915e112e748b86e7885d680a7b0c57875
atsuya23 <90808047+atsuya23@users.noreply.github.com>
2021-09-19 13:22:24 +0900

    完了、削除ボタンの生成（機能はまだ）

--- src/index.js
+++ src/index.js
@@ -12,9 +12,24 @@ const onClickAdd = () => {
   const li = document.createElement("li");
   li.innerText = inputText;
 
+  // button(完了)タグせいｓ
+  const doneButton = document.createElement("button");
+  doneButton.innerText = "完了";
+  doneButton.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  // button(削除)タグせいｓ
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    alert("削除");
+  });
+
   // divタグの子要素に各要素を設定
   div.appendChild(li);
-  console.log(div);
+  div.appendChild(doneButton);
+  div.appendChild(deleteButton);
 
   // 未完了のリストに追加
   document.getElementById("undone-list").appendChild(div);
